65e5919ff675f9a4fd60ff0ad2d383c37f2fc6ed
crixue <crixue@qq.com>
2024-08-02 21:14:41 +0800

    upgrade ant version

--- deploy_on_dev.sh
+++ deploy_on_dev.sh
@@ -2,6 +2,7 @@
 
 git pull
 
+#docker build --no-cache -t art.local:8081/docker-local/lingro-web:latest .
 docker build -t art.local:8081/docker-local/lingro-web:latest .
 
 docker push art.local:8081/docker-local/lingro-web:latest
